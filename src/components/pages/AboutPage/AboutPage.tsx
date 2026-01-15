08ea8d75bdfaeb6f264d0525ca3ce8f998642d73
Dean Kim <deankim1118@gmail.com>
2026-01-15 18:25:56 -0500

    About Chairman - Add Horo Section

---
+++ src/components/pages/AboutPage/AboutPage.tsx
@@ -0,0 +1,15 @@
+import { PageHero } from '@/components/PageHero';
+
+export function AboutPage() {
+  return (
+    <>
+      <PageHero
+        title='Washington Jabez University'
+        subtitle='Welcome'
+        imageUrl='/wju-hero-img.png'
+        imageAlt='Winchester Jung University campus'
+      />
+      {/* 추가 섹션들은 여기에 추가됩니다 */}
+    </>
+  );
+}
